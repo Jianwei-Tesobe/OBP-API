d63594fe40331711562c7524a6ed21caea5f3a81
Jianwei-Tesobe <jianwei@tesobe.com>
2018-11-21 13:55:16 +0100

    added kafka test for obp topics

--- src/test/scala/code/kafka/KafkaTest.scala
+++ src/test/scala/code/kafka/KafkaTest.scala
@@ -1,80 +1,40 @@
 package code.kafka
 
-import bootstrap.liftweb.Boot
-import code.bankconnectors.vMar2017.{InboundBank, InboundStatusMessage}
+import code.api.util.CallContext
+import code.bankconnectors.vMar2017.{Bank2, InboundBank, InboundStatusMessage}
 import code.bankconnectors.vSept2018._
-import code.util.Helper.MdcLoggable
+import code.setup.KafkaSetup
+import net.liftweb.common.{Box, Full}
 import net.liftweb.json
-import net.liftweb.json.{DefaultFormats, Extraction}
-import net.manub.embeddedkafka.{EmbeddedKafka, EmbeddedKafkaConfig}
-import org.apache.kafka.clients.producer.ProducerRecord
-import org.apache.kafka.common.serialization.{StringDeserializer, StringSerializer}
-import org.scalatest.{FeatureSpec, _}
 
 import scala.collection.immutable.List
-import scala.concurrent.duration._
-import scala.concurrent.Await
 
-class KafkaTest extends FeatureSpec with EmbeddedKafka with KafkaHelper
-  with BeforeAndAfterEach with GivenWhenThen
-  with BeforeAndAfterAll
-  with Matchers with MdcLoggable {
+class KafkaTest extends KafkaSetup {
 
-  override def beforeAll(): Unit = {
-    super.beforeAll()
-    new Boot().boot
-  }
-
-  implicit val formats = DefaultFormats
-  implicit val config = EmbeddedKafkaConfig(kafkaPort = 9092, zooKeeperPort = 2181)
-  implicit val stringSerializer = new StringSerializer
-  implicit val stringDeserializer = new StringDeserializer
-
-  def runWithKafka(runner: => Unit): Unit = {
-    withRunningKafka {
-      createCustomTopic("Request", Map.empty, 10, 1)
-      createCustomTopic("Response", Map.empty, 10, 1)
-      runner
+  feature("Send and retrieve message") {
+    scenario("Send and retrieve message directly to and from kafka") {
+      val emptyStatusMessage = InboundStatusMessage("", "", "", "")
+      val inBound = InboundGetBanks(AuthInfo(), Status("", List(emptyStatusMessage)), List(InboundBank("1", "2", "3", "4")))
+      When("send a OutboundGetBanks message")
+      val result:json.JValue = runWithKafkaFuture(inBound) {
+        val req = OutboundGetBanks(AuthInfo())
+        processToFuture[OutboundGetBanks](req)
+      }
+      val banks = result.extract[InboundGetBanks]
+      banks should be equals (inBound)
     }
-  }
 
-   feature("Send and retrieve message") {
-//    scenario("Send and retrieve simple") {
-//      runWithKafka {
-//        When("send a string message to topic: someTopic")
-//        publishStringMessageToKafka("someTopic", "some message=====================")
-//        Then("get the message from topic: someTopic")
-//        val str = consumeFirstStringMessageFrom("someTopic")
-//        str.shouldBe("some message=====================")
-//      }
-//    }
+    scenario("Send and retrieve api message") {
+      When("send a OutboundGetBanks api message")
+      val emptyStatusMessage = InboundStatusMessage("", "", "", "")
+      val singleInboundBank = List(InboundBank("1", "2", "3", "4"))
+      val inBound = InboundGetBanks(AuthInfo(), Status("", List(emptyStatusMessage)), singleInboundBank)
 
-
-        scenario("Send and retrieve api message") {
-          runWithKafka {
-            When("send a string api message")
-            val topics = Topics.createTopicByClassName("OutboundGetBanks")
-            val emptyStatusMessage = InboundStatusMessage("", "", "", "")
-            val inBound = InboundGetBanks(AuthInfo(), Status("", List(emptyStatusMessage)), List(InboundBank("1", "2", "3", "4")))
-
-          val req = OutboundGetBanks(AuthInfo())
-          val inBoundStr = json.compactRender(Extraction.decompose(inBound))
-
-//            val eventualValue = KafkaMappedConnector_vSept2018.processToBox[OutboundGetBanks](req)
-//            val eventualValue = KafkaMappedConnector_vSept2018.getBanksFuture(None)
-          val eventualValue = processToFuture[OutboundGetBanks](req)
-//            Thread.sleep(1000)
-            val keyAndPayload = consumeFirstKeyedMessageFrom[String, String](topics.request)
-            println("xxx:"+keyAndPayload)
-            publishToKafka(topics.response, keyAndPayload._1, inBoundStr)
-            Await.result(eventualValue, (10 second))
-            val serializable = eventualValue.value.getOrElse("").extract[InboundGetBanks]
-            println(serializable + "-----------------------------------------------")
-          }
-        }
-     
-//     connector=kafka_vSept2018
-//api_instance_id=1
-//remotedata.timeout=30
+      val result:Box[(List[Bank2], Option[CallContext])] = runWithKafkaFuture(inBound) {
+        KafkaMappedConnector_vSept2018.getBanksFuture(None)
+      }
+      val expectResult = Full(singleInboundBank.map(Bank2))
+      result should be equals(expectResult)
+    }
   }
 }
---
+++ src/test/scala/code/setup/KafkaSetup.scala
@@ -0,0 +1,120 @@
+package code.setup
+
+import bootstrap.liftweb.Boot
+import code.kafka.{KafkaConsumer, KafkaHelper, NorthSideConsumer, Topics}
+import code.util.Helper.MdcLoggable
+import net.liftweb.json
+import net.liftweb.json.{DefaultFormats, Extraction}
+import scala.concurrent.ExecutionContext.Implicits.global
+import net.manub.embeddedkafka.{EmbeddedKafka, EmbeddedKafkaConfig}
+import org.apache.kafka.common.serialization.{StringDeserializer, StringSerializer}
+import org.scalatest.{FeatureSpec, _}
+
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+
+trait KafkaSetup extends FeatureSpec with EmbeddedKafka with KafkaHelper
+  with BeforeAndAfterEach with GivenWhenThen
+  with BeforeAndAfterAll
+  with Matchers with MdcLoggable {
+
+
+
+  implicit val formats = DefaultFormats
+  implicit val config = EmbeddedKafkaConfig(kafkaPort = 9092) //TODO the port should read from test.default.props, but fail
+  implicit val stringSerializer = new StringSerializer
+  implicit val stringDeserializer = new StringDeserializer
+
+  lazy val requestMapResponseTopics:Map[String, String] =  NorthSideConsumer.listOfTopics
+    .map(Topics.createTopicByClassName)
+    .map(pair => (pair.request, pair.response))
+    .toMap
+  lazy val requestTopics = requestMapResponseTopics.keySet
+
+  override def beforeAll(): Unit = {
+    super.beforeAll()
+    KafkaConsumer.primaryConsumer.started = true
+    KafkaConsumer.primaryConsumer.complete()
+    new Boot().boot
+  }
+
+
+  def runWithKafka[U](inBound: AnyRef, waitTime: Duration = (10 second))(runner: => U): U = {
+    KafkaConsumer.primaryConsumer.started = false
+    KafkaConsumer.primaryConsumer.completed = false
+
+    try {
+      EmbeddedKafka.start() //TODO should in beforeEvery and afterEvery, but they are not called, strange.
+      createCustomTopic("Request", Map.empty, 10, 1)
+      createCustomTopic("Response", Map.empty, 10, 1)
+      KafkaConsumer.primaryConsumer.start()
+
+      // here is async, this means response is not send before request
+      dispathResponse(inBound)
+      runner
+    } finally {
+      KafkaConsumer.primaryConsumer.complete()
+      Thread.sleep(150)
+      EmbeddedKafka.stop()
+    }
+  }
+  def runWithKafkaFuture[U](inBound: AnyRef, waitTime: Duration = (10 second))(runner: => Future[U]): U = {
+    this.runWithKafka[U](inBound, waitTime) {
+      Await.result(runner, waitTime)
+    }
+  }
+
+  /**
+    * send an object to kafka as response
+    *
+    * @param inBound inBound object that will send to kafka as a response
+    * @tparam T Outbound type
+    */
+  def dispathResponse(inBound: AnyRef): Unit = {
+    val inBoundStr = json.compactRender(Extraction.decompose(inBound))
+    Future{
+      val requestKeyValue = consumeNumberKeyedMessagesFromTopics(requestTopics, 1, true)
+      val (requestTopic, keyValueList) = requestKeyValue.find(_._2.nonEmpty).get
+      val (key, _) = keyValueList.head
+      val responseTopic = requestMapResponseTopics(requestTopic)
+      publishToKafka(responseTopic, key, inBoundStr)
+    }
+  }
+
+  /**
+    * retrieve InBound object from Future
+    *
+    * @param response response Future
+    * @param waitTime wait time
+    * @tparam T InBound type
+    * @return InBOund object to do assert
+    */
+  def getInBound[T: Manifest](response: Future[json.JValue], waitTime: Duration = (10 second)) = {
+    val value = Await.result(response, waitTime)
+    value.extract[T]
+  }
+
+  /**
+    * get response after call api
+    *
+    * @param inBound  inBound object that return from kafka
+    * @param waitTime max wait time to continue process after call the api
+    * @param apiCall  call api method
+    * @tparam T OutBound tye
+    * @tparam D api return concrete object in the api return Future
+    * @return extracted object from api future result
+    */
+  //  def getResponseByApi[T <: TopicTrait : ClassTag, D]
+  //    (inBound: AnyRef, waitTime: Duration = (10 second))
+  //    (apiCall: =>  Future[Box[(D, Option[CallContext])]]): Box[(D, Option[CallContext])] = {
+  //    return runWithKafka[T]() {
+  //      val futureResult =  apiCall
+  //      dispathResponse[T](inBound)
+  //      return Await.result(futureResult, waitTime)
+  //    }
+  //  }
+
+  //     connector=kafka_vSept2018
+  //api_instance_id=1
+  //remotedata.timeout=30
+}
