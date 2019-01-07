19e366ee00831eaf5dfd01be54c22fe7fcf0581f
jianwei-tesobe <jianwei@tesobe.com>
2019-01-07 22:33:37 +0800

    finish kafka test

--- src/test/scala/code/kafka/KafkaTest.scala
+++ src/test/scala/code/kafka/KafkaTest.scala
@@ -8,18 +8,27 @@ import net.liftweb.common.{Box, Full}
 import net.liftweb.json
 
 import scala.collection.immutable.List
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
+import scala.concurrent.duration._
 
 class KafkaTest extends KafkaSetup {
+  val waitTime: Duration = (10 second)
+
 
   feature("Send and retrieve message") {
     scenario("Send and retrieve message directly to and from kafka") {
       val emptyStatusMessage = InboundStatusMessage("", "", "", "")
       val inBound = InboundGetBanks(AuthInfo(), Status("", List(emptyStatusMessage)), List(InboundBank("1", "2", "3", "4")))
       When("send a OutboundGetBanks message")
-      val result:json.JValue = runWithKafkaFuture(inBound) {
-        val req = OutboundGetBanks(AuthInfo())
-        processToFuture[OutboundGetBanks](req)
-      }
+
+      dispathResponse(inBound)
+      val req = OutboundGetBanks(AuthInfo())
+
+      val future = processToFuture[OutboundGetBanks](req)
+      val result:json.JValue =  Await.result(future, waitTime)
+
       val banks = result.extract[InboundGetBanks]
       banks should be equals (inBound)
     }
@@ -30,10 +39,12 @@ class KafkaTest extends KafkaSetup {
       val singleInboundBank = List(InboundBank("1", "2", "3", "4"))
       val inBound = InboundGetBanks(AuthInfo(), Status("", List(emptyStatusMessage)), singleInboundBank)
 
-      val result:Box[(List[Bank2], Option[CallContext])] = runWithKafkaFuture(inBound) {
-        KafkaMappedConnector_vSept2018.getBanksFuture(None)
-      }
+      dispathResponse(inBound)
+
+      val future = KafkaMappedConnector_vSept2018.getBanksFuture(None)
+      val result:Box[(List[Bank2], Option[CallContext])] =  Await.result(future, waitTime)
       val expectResult = Full(singleInboundBank.map(Bank2))
+
       result should be equals(expectResult)
     }
   }
